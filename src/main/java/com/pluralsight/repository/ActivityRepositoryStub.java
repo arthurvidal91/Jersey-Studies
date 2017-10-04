a786087570a366184f701e69fb95d83e3feca956
Arthur Vidal <arthurvidal91@gmail.com>
2017-10-04 15:48:09 -0300

    First commit

---
+++ src/main/java/com/pluralsight/repository/ActivityRepositoryStub.java
@@ -0,0 +1,115 @@
+package com.pluralsight.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.pluralsight.model.Activity;
+import com.pluralsight.model.ActivitySearch;
+import com.pluralsight.model.User;
+
+public class ActivityRepositoryStub implements ActivityRepository {
+	
+
+	@Override
+	public List<Activity> findByConstraints(ActivitySearch search) {
+		
+		System.out.println(search.getDurationFrom());
+		System.out.println(search.getSearchType());
+		
+		List<Activity> activities = new ArrayList<>();
+		Activity activity =  new Activity();
+		activity.setId("345");
+		activity.setDescription("Swimming");
+		activity.setDuration(55);
+		
+		activities.add(activity);
+		return activities;
+	}
+
+
+	@Override
+	public List<Activity> findByDescriptionAndDurationRange(List<String> descriptions, 
+															int durationFrom,
+															int durationTo) {
+		//select * from activities 	where description in (?,?,...) and duration > ? and duration < ?
+		
+		List<Activity> activities = new ArrayList<>();
+		Activity activity =  new Activity();
+		activity.setId("345");
+		activity.setDescription("Swimming");
+		activity.setDuration(55);
+		
+		activities.add(activity);
+		return activities;
+	}
+	
+	@Override
+	public List<Activity> findByDescription(List<String> descriptions) {
+		//select * from activities where description in (?,?,?...)
+		List<Activity> activities = new ArrayList<>();
+		Activity activity =  new Activity();
+		activity.setId("345");
+		activity.setDescription("Swimming");
+		activity.setDuration(55);
+		
+		activities.add(activity);
+		return activities;
+	}
+	
+	@Override
+	public List<Activity> findAllActivities() {
+		List<Activity> activities = new ArrayList<>();
+
+		Activity activity1 = new Activity();
+		activity1.setDescription("Swimming");
+		activity1.setDuration(55);
+		activities.add(activity1);
+
+		Activity activity2 = new Activity();
+		activity2.setDescription("Cycling");
+		activity2.setDuration(120);
+		activities.add(activity2);
+
+		return activities;
+	}
+
+	@Override
+	public Activity findActivity(String activityId) {
+
+		if (activityId.equals("7777")) {
+			return null;
+		}
+
+		Activity activity1 = new Activity();
+		activity1.setId(activityId);
+		activity1.setDescription("Swimming");
+		activity1.setDuration(55);
+
+		User user = new User();
+		user.setId("5678");
+		user.setName("Arthur");
+		activity1.setUser(user);
+
+		return activity1;
+	}
+
+	@Override
+	public void create(Activity activity) {
+		// should issue a insert statement to the db
+	}
+
+	@Override
+	public Activity update(Activity activity) {
+		/*
+		 * search the database to see if we have an activity with that ID already if
+		 * resultset size == 0 insert into Activity table else update the Activity
+		 */
+		return activity;
+	}
+
+	@Override
+	public void delete(String activityId) {
+		// delete from activity where id = String activityId
+	}
+
+}
